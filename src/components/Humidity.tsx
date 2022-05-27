6297d52ca9f71be71450b1e4ac8ae55b1d38bbd1
FirminoMassango <firminorafael0@gmail.com>
2022-05-27 16:36:35 +0200

    Modifying the humidity progressbar

--- src/components/Humidity.tsx
+++ src/components/Humidity.tsx
@@ -1,38 +1,4 @@
-import { useEffect, useState } from "react";
-
 function Humidity(props: any) {
-  const [progressFill, setProgressFill] = useState<string>(
-    "bg-progress h-2 rounded-lg"
-  );
-
-  const humidity = props.humidity;
-
-  useEffect(() => {
-    if (humidity == 0) {
-      setProgressFill("bg-white h-2 rounded-lg");
-    } else if (humidity <= 10) {
-      setProgressFill("bg-progress w-1/12 h-2 rounded-lg");
-    } else if (humidity <= 20) {
-      setProgressFill("bg-progress w-1/5 h-2 rounded-lg");
-    } else if (humidity <= 30) {
-      setProgressFill("bg-progress w-1/3 h-2 rounded-lg");
-    } else if (humidity <= 40) {
-      setProgressFill("bg-progress w-2/5 h-2 rounded-lg");
-    } else if (humidity <= 50) {
-      setProgressFill("bg-progress w-1/2 h-2 rounded-lg");
-    } else if (humidity <= 60) {
-      setProgressFill("bg-progress w-3/5 h-2 rounded-lg");
-    } else if (humidity <= 70) {
-      setProgressFill("bg-progress w-3/4 h-2 rounded-lg");
-    } else if (humidity <= 80) {
-      setProgressFill("bg-progress w-4/5 h-2 rounded-lg");
-    } else if (humidity <= 90) {
-      setProgressFill("bg-progress w-11/12 h-2 rounded-lg");
-    } else if (humidity == 100) {
-      setProgressFill("bg-progress w-full h-2 rounded-lg");
-    }
-  }, []);
-
   return (
     <div className="bg-left flex flex-col justify-center w-full h-44">
       <header className="text-white text-center">Humidity</header>
@@ -46,7 +12,10 @@ function Humidity(props: any) {
           <span>100</span>
         </div>
         <div className="bg-white w-full h-2 rounded-lg">
-          <div className={progressFill}></div>
+          <div
+            className="bg-progress h-2 rounded-lg"
+            style={{ width: `${props.humidity}%` }}
+          ></div>
         </div>
         <span className="flex justify-end pt-1">%</span>
       </footer>
