b32298cb7f8dd9ca5f09e2f2e91d775b2ff84926
FirminoMassango <firminorafael0@gmail.com>
2022-05-27 15:08:41 +0200

    change icon

--- src/components/WindStatus.tsx
+++ src/components/WindStatus.tsx
@@ -5,12 +5,12 @@ function WindStatus(props: any) {
       <main className="font-black  text-white text-center my-6">
         <span className="text-5xl">{props.windSpeed}</span> <span>m/s</span>
       </main>
-      <footer className="text-white text-center flex justify-center content-center">
+      <footer className="text-white text-center flex justify-center items-center">
         <span
-          className="material-icons text-lg text-dark-200  mr-5"
-          style={{ transform: `rotate(${props.windDirectionDeg}deg)` }}
+          className="material-icons text-2xl text-dark-200  mr-5"
+          style={{ transform: `rotate(${-props.windDirectionDeg}deg)` }}
         >
-          send
+          arrow_circle_left
         </span>
         <span>{props.windDirection}</span>
       </footer>
