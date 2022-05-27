638aea21e422d6784e9d11b3870f1f3ed60b5ded
FirminoMassango <firminorafael0@gmail.com>
2022-05-27 20:05:37 +0200

    Remove the footer text to a proper component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="text-footer text-center mb-5">
+      created by{" "}
+      <span className="underline font-semibold">Firmino Massango</span> -
+      devChallenges.io
+    </footer>
+  );
+}
+
+export default Footer;
