c742d5a356ea88d806389c3d108e547aa9c57f8e
FirminoMassango <firminorafael0@gmail.com>
2022-05-27 15:59:54 +0200

    Change icon size

--- src/components/cloud_icons/sm/ScatteredCloudsIcon-sm.tsx
+++ src/components/cloud_icons/sm/ScatteredCloudsIcon-sm.tsx
@@ -2,7 +2,7 @@ import scatteredClouds from "../../../assets/HeavyCloud.png";
 
 function ScatteredCloudsIcon() {
   return (
-    <div id="2" className="flex justify-center my-2">
+    <div id="2" className="flex justify-center my-5">
       <img src={scatteredClouds} className="w-14" alt="Scattered Clouds" />
     </div>
   );
