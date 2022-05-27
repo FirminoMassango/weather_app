43cf1fca664fc0ed9e82e02fe6b018066f6fde51
FirminoMassango <firminorafael0@gmail.com>
2022-05-27 07:14:16 +0200

    Change http request to https

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const [humidity, setHumidity] = useState<number>(0);
   const [weatherProps, setWeatherProps] = useState<RootObject>();
 
-  const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&q=Maputo&appid=e81a6836bb1ca7c059ad403ca41b2dbe`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?id=524901&q=${city}&appid=${token}`;
 
   async function fetchWeatherProps() {
     const response = await fetch(url);
@@ -34,15 +34,18 @@ function App() {
   }, []);
 
   return (
-    <div className="flex h-full">
-      <div id-="left" className="w-1/3  bg-left text-white font-raleway">
+    <div className="flex flex-col md:flex-row h-full ">
+      <div
+        id-="left"
+        className="w-full md:w-1/3  bg-left text-white font-raleway"
+      >
         <LeftPanel
           temp={weatherProps?.list[0].main.temp_max}
           description={weatherProps?.list[0].weather[0].description}
           city={weatherProps?.city.name}
         />
       </div>
-      <div id="right" className="w-2/3 bg-right overflow-scroll font-raleway">
+      <div id="right" className="w-full md:w-2/3 bg-right font-raleway">
         <div id="container" className="mx-32 my-10">
           <header className="flex justify-end mb-5">
             <SwitchScale />
@@ -64,11 +67,11 @@ function App() {
           <h1 className="my-10 font-bold text-white text-xl">
             Today’s Highlights
           </h1>
-          <div className="grid grid-flow-col gap-10">
+          <div className="grid grid-rows-2 md:grid-rows-1 grid-flow-col gap-10">
             <WindStatus windSpeed={weatherProps?.list[0].wind.speed} />
             <Humidity humidity={humidity} />
           </div>
-          <div className="my-10 grid grid-flow-col gap-10">
+          <div className="w-full my-10 grid grid-rows-2 md:grid-rows-1 grid-flow-col gap-10">
             <Visibility visibility={weatherProps?.list[0].visibility} />
             <AirPressure airPressure={weatherProps?.list[0].main.pressure} />
           </div>
